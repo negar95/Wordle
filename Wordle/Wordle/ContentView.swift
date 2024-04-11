4cad50c7dbc0f188968ad3d4c13135c4ccb5769d
negar m <negar.moshtaghi@gmail.com>
2024-04-11 23:10:54 +0330

    add draggable keyboard and droppable word

--- Wordle/Wordle/ContentView.swift
+++ Wordle/Wordle/ContentView.swift
@@ -6,19 +6,51 @@
 //
 
 import SwiftUI
+import Combine
+import UniformTypeIdentifiers
 
 struct ContentView: View {
+
+    @State var keyboardModels: [[KeyModel]]
+    @State var word: [KeyModel]
+
+    init(word: String) {
+        self.keyboardModels = KeyboardModelGenerator.generate()
+        self.word = word.map { _ in KeyModel(character: "") }
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            HStack {
+                ForEach(word) { model in
+                    KeyView(model: model)
+                        .onDrop(of: [.plainText], isTargeted: nil) { providers, location in
+                            providers.first?.loadDataRepresentation(forTypeIdentifier: UTType.plainText.identifier) { data, error in
+                                if let data = data, let content = String(data: data, encoding: .utf8) {
+                                    if let index = word.firstIndex(where: {$0.id == model.id}) {
+                                        word[index].character = content
+                                    }
+                                }
+                            }
+                            return true
+                        }
+                }
+            }
+            Spacer()
+            VStack {
+                ForEach(keyboardModels, id: \.self) { row in
+                    HStack {
+                        ForEach(row) { model in
+                            KeyView(model: model)
+                                .onDrag {
+                                    return NSItemProvider(object: String(model.character) as NSString)
+                                }
+                        }
+                    }
+                }
+            }
+            Spacer()
         }
-        .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
---
+++ Wordle/Wordle/Views/SwiftUIView.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftUIView.swift
+//  Wordle
+//
+//  Created by Negar Moshtaghi on 4/2/24.
+//
+
+import SwiftUI
+
+
+struct SwiftUIView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Spacer()
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
